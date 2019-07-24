350e75f8abe462f36a161b374dbaa70b56441883
Jeremy Antonoff <jeremyantonoff@gmail.com>
2019-07-23 20:24:22 -0400

    add authorization controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NomoBucket.API.Data;
+using NomoBucket.API.Dtos;
+using NomoBucket.API.Models;
+
+namespace NomoBucket.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _repo;
+    private readonly IConfiguration _config;
+
+    public AuthController(IAuthRepository repo, IConfiguration config)
+    {
+      _repo = repo;
+      _config = config;
+    }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserRegistrationDto userRegistrationDto)
+    {
+      userRegistrationDto.Username = userRegistrationDto.Username.ToLower();
+
+      if (await _repo.CheckExistingUser(userRegistrationDto.Username))
+      {
+        return BadRequest("User already exists");
+      }
+      var userToCreate = new User();
+      userToCreate.Username = userRegistrationDto.Username;
+
+      var createdUser = _repo.Register(userToCreate, userRegistrationDto.Password);
+      if (createdUser == null)
+      {
+        return BadRequest("Could not create user");
+      }
+      return StatusCode(201);
+    }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+    {
+      userLoginDto.Username = userLoginDto.Username.ToLower();
+      var user = await _repo.Login(userLoginDto.Username, userLoginDto.Password);
+      if (user == null)
+      {
+        return Unauthorized();
+      }
+      var claims = new[]
+      {
+          new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+          new Claim(ClaimTypes.Name, user.Username)
+      };
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Authorization:TokenSecret").Value));
+      var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = credentials
+
+      };
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return Ok(new
+      {
+        token = tokenHandler.WriteToken(token)
+      });
+
+    }
+  }
+}
