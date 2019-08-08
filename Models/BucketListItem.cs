7e333bd3b566299dfba26bdccd4d2a06c2cb2154
Jeremy Antonoff <jeremyantonoff@gmail.com>
2019-08-07 23:11:50 -0400

    migrations and models for bucket list

---
+++ Models/BucketListItem.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace NomoBucket.API.Models
+{
+    public class BucketListItem
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; }
+        public string CompletedPhotoUrl { get; set; }
+
+        public bool Completed { get; set; }
+        public DateTime CompletedAt { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+
+
+    }
+}
