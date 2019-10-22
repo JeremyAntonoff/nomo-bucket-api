09b09b72a65dea63128fa84081470d3bbce50048
Jeremy Antonoff <jantonoff@Jeremys-MacBook-Pro.local>
2019-10-22 15:52:17 -0400

    add Follow to context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,14 +1,34 @@
 using Microsoft.EntityFrameworkCore;
+using nomo_bucket_api.Models;
 using NomoBucket.API.Models;
 
 namespace NomoBucket.API.Data
 {
-  public class DataContext : DbContext
-  {
-    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-    public DbSet<User> Users { get; set; }
-    public DbSet<BucketListItem> BucketListItems {get; set;}
-    public DbSet<FeedItem> FeedItems {get; set;}
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BucketListItem> BucketListItems { get; set; }
+        public DbSet<FeedItem> FeedItems { get; set; }
+        public DbSet<Follow> Follows { get; set; }
 
-  }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Follow>()
+            .HasKey(k => new { k.FollowerId, k.FolloweeId });
+
+            builder.Entity<Follow>()
+            .HasOne(u => u.Followee)
+            .WithMany(u => u.Followers)
+            .HasForeignKey(u => u.FolloweeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Follow>()
+            .HasOne(u => u.Follower)
+            .WithMany(u => u.Followees)
+            .HasForeignKey(u => u.FollowerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
 }
