8fe09256e26f4f5bef57d0a6ac8cbee261f26b64
Jeremy Antonoff <jantonoff@Jeremys-MacBook-Pro.local>
2019-10-22 15:53:23 -0400

    add follow model

---
+++ Models/Follow.cs
@@ -0,0 +1,12 @@
+using NomoBucket.API.Models;
+
+namespace nomo_bucket_api.Models
+{
+    public class Follow
+    {
+        public int FollowerId { get; set; }
+        public int FolloweeId { get; set; }
+        public User Follower { get; set; }
+        public User Followee { get; set; }
+    }
+}
