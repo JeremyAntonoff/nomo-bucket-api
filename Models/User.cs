02d4b3bc55a9f8257ce8fa838ffa7adb3fccc286
Jeremy Antonoff <jantonoff@Jeremys-MacBook-Pro.local>
2019-10-22 15:49:59 -0400

    add follow to User Model

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using nomo_bucket_api.Models;
 
 namespace NomoBucket.API.Models
 {
@@ -15,10 +16,12 @@ namespace NomoBucket.API.Models
         public DateTime DateOfBirth { get; set; }
         public DateTime LastActive { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string Country { get; set; } 
+        public string Country { get; set; }
         public string PhotoUrl { get; set; }
         public string PublicPhotoId { get; set; }
         public ICollection<BucketListItem> BucketList { get; set; }
-        
+        public ICollection<Follow> Followers { get; set; }
+        public ICollection<Follow> Followees { get; set; }
+
     }
 }
